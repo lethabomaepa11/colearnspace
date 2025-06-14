ea7738a66fe156165bfe01212e8a8e1fe08259b5
lethabo <shysnylethabo11@gmail.com>
2025-06-15 00:12:28 +0200

    fix: fixed the create links, made challenge submissions to be displayed even when submissions aren't open

--- src/routes/portal/challenges/[challenge_slug]/submissions/+page.svelte
+++ src/routes/portal/challenges/[challenge_slug]/submissions/+page.svelte
@@ -19,33 +19,38 @@
 	<meta property="og:title" content="{data.challenge.title} Submissions | ColearnSpace" />
 </svelte:head>
 
-{#if moment(challenge.start_date).isAfter(moment())}
-	<h1 class="text-xl font-bold">Submissions will open {moment(challenge.start_date).fromNow()}</h1>
-{:else if moment(challenge.end_date).isSameOrBefore(moment())}
-	<h1 class="text-xl font-bold">Submissions are closed</h1>
-{:else}
-	<main transition:slide>
+<main transition:slide>
+	{#if moment(challenge.start_date).isAfter(moment())}
+		<h1 class="text-xl font-bold">
+			Submissions will open {moment(challenge.start_date).fromNow()}
+		</h1>
+	{:else if moment(challenge.end_date).isSameOrBefore(moment())}
+		<h1 class="text-xl font-bold">Submissions are closed</h1>
+	{:else}
 		<section class="flex w-full items-center justify-between">
 			<h1 class="text-2xl font-bold">Submissions</h1>
 			<a href="/portal/challenges/{data.challenge.slug}/submissions/new" class="btn btn-primary"
 				>New Submission</a
 			>
 		</section>
-		<article class="my-5 grid grid-cols-2 gap-3 lg:grid-cols-4">
-			{#each submissions as submission}
-				<a
-					href="/portal/challenges/{challenge.slug}/submissions/{submission.id}"
-					class="card bg-base-200 border-base-300 w-full border"
-				>
-					<div class="card-body">
-						<h4 class="">{submission.title}</h4>
-						<p class="text-xs">
-							By {submission.user.name}
-							{moment(submission.created_at).fromNow()}
-						</p>
-					</div>
-				</a>
-			{/each}
-		</article>
-	</main>
-{/if}
+	{/if}
+	<article class="my-5 grid grid-cols-2 gap-3 lg:grid-cols-4">
+		{#if submissions.length === 0}
+			<p>No Submissions</p>
+		{/if}
+		{#each submissions as submission}
+			<a
+				href="/portal/challenges/{challenge.slug}/submissions/{submission.id}"
+				class="card bg-base-200 border-base-300 w-full border"
+			>
+				<div class="card-body">
+					<h4 class="">{submission.title}</h4>
+					<p class="text-xs">
+						By {submission.user.name}
+						{moment(submission.created_at).fromNow()}
+					</p>
+				</div>
+			</a>
+		{/each}
+	</article>
+</main>
